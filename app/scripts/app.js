02e6a53b07e04ee013b5b1231dc77fe0dec75657
Paul FIORENTINO <pfiorentino@tellmeplus.com>
2016-10-26 23:35:33 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name awsinstancesManagerApp
+ * @description
+ * # awsinstancesManagerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('awsinstancesManagerApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name awsinstancesManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the awsinstancesManagerApp
+ */
+angular.module('awsinstancesManagerApp')
+  .controller('MainCtrl', function () {
+    
+  });
